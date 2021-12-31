f1eac9b24e5aa6d7337d0255a575fd03b56cd1b7
pe.khorsandi <pe.khorsandi@gmail.com>
2021-12-31 22:01:49 +0330

    More Control Braces Fixed

--- src/main/java/semantic/symbol/SymbolTable.java
+++ src/main/java/semantic/symbol/SymbolTable.java
@@ -83,8 +83,8 @@ public class SymbolTable {
 
     public Symbol get(String className, String methodName, String variable) {
         Symbol res = klasses.get(className).Methodes.get(methodName).getVariable(variable);
-        if (res == null)
-            res = get(variable, className);
+        if (res == null) {
+            res = get(variable, className); }
         return res;
     }
 
@@ -165,10 +165,10 @@ public class SymbolTable {
         }
 
         public Symbol getVariable(String variableName) {
-            if (parameters.containsKey(variableName))
-                return parameters.get(variableName);
-            if (localVariable.containsKey(variableName))
-                return localVariable.get(variableName);
+            if (parameters.containsKey(variableName)) {
+                return parameters.get(variableName); }
+            if (localVariable.containsKey(variableName)) {
+                return localVariable.get(variableName); }
             return null;
         }
 
